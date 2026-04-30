2159b9cf6618bbbfd1d0b177cfc8b4c5b9e4b55a
general-bean <66106767+general-bean@users.noreply.github.com>
2026-04-30 14:35:30 +0800

    Update diy1.sh

--- 237/diy1.sh
+++ 237/diy1.sh
@@ -3,7 +3,7 @@
 # git clone https://github.com/sirpdboy/luci-app-timecontrol package/luci-app-timecontrol
 
 # 一键分区
-git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+# git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 
 
 # 任务设置taskplan
@@ -23,6 +23,6 @@ git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-part
 git clone https://github.com/linkease/istore.git package/istore
 
 # lucky大吉
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+# git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 
 
