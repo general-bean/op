fad8f83a36936d027cea41e8f4759d59ce8d40c1
general-bean <66106767+general-bean@users.noreply.github.com>
2026-05-03 10:10:50 +0800

    Delete Amlogic directory

--- Amlogic/diy1.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
-git clone https://github.com/linkease/istore.git package/istore
-git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
-
--- Amlogic/lede.sh
+++
@@ -1,5 +0,0 @@
-rm -rf package/luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
-git clone https://github.com/linkease/istore.git package/istore
-
