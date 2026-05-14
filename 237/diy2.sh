47933aa469cb53a9f85e877818e7b1ee5316e1b4
general-bean <66106767+general-bean@users.noreply.github.com>
2026-05-14 21:10:24 +0800

    Update diy2.sh

--- 237/diy2.sh
+++ 237/diy2.sh
@@ -1,2 +1,3 @@
 sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # sed -i 's/ImmortalWrt/Netcore/g' package/base-files/files/bin/config_generate 
+sed -i 's/Factory/factory/g' package/mtk/drivers/conninfra/src/platform/mt7986/mt7986_pos.c
