baa0a736fd8fcdc029f18b3928c7813a197c159e
general-bean <66106767+general-bean@users.noreply.github.com>
2026-05-16 19:25:55 +0800

    Update diy2.sh

--- 237/diy2.sh
+++ 237/diy2.sh
@@ -1,4 +1,4 @@
 sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # sed -i 's/ImmortalWrt/Netcore/g' package/base-files/files/bin/config_generate 
-# sed -i 's/Factory/factory/g' package/mtk/drivers/conninfra/src/platform/mt7986/mt7986_pos.c
-# sed -i 's/font-size: 1.8rem/font-size: 1.4rem/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+sed -i 's/Factory/factory/g' package/mtk/drivers/conninfra/src/platform/mt7986/mt7986_pos.c
+sed -i 's/font-size: 1.8rem/font-size: 1.4rem/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
